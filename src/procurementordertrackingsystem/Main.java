3cb0113afa1718c6a5f1b2efd52363de1439187f
Tarek Hamwi <tareqhamwi85@gmail.com>
2024-12-08 15:11:02 +0800

    testing the verified account in github (useless commit)

--- src/procurementordertrackingsystem/Main.java
+++ src/procurementordertrackingsystem/Main.java
@@ -2,7 +2,6 @@ package procurementordertrackingsystem;
 
 
 import procurementordertrackingsystem.entities.LoginPage;
-import procurementordertrackingsystem.roles.InventoryManager;
 
 public class Main {
     public static void main(String[] args) {
