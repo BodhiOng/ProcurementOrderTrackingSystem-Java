96d1e82becdafd87f976e7db020b0529d5a32574
Realxeng <realxengpandelaki@gmail.com>
2024-12-14 21:32:15 +0800

    Added displayOneSaleById to display one record of sales entry

--- src/procurementordertrackingsystem/entities/SalesEntry.java
+++ src/procurementordertrackingsystem/entities/SalesEntry.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import procurementordertrackingsystem.utilities.CRUDOntoFile;
@@ -33,7 +34,9 @@ public class SalesEntry implements IDGenerator{
     //Override the generateID method from IDGenerator to generate a sequential ID
     public String generateID() {
         List<String> rawdata = getAllSales();
-        return String.format("SE%04d", rawdata.size()+1);
+        String[] onesale = rawdata.getLast().split(",");
+        int lastid = Integer.parseInt(onesale[0].substring(2));
+        return String.format("SE%04d", lastid+1);
     }
     
     //Method to read one sales record given its ID
@@ -41,13 +44,27 @@ public class SalesEntry implements IDGenerator{
         List<String> rawdata = getAllSales();
         List<String> onerecord = new ArrayList<>();
         String[] line = null;
+        String[] toreturn = null;
         for (String lines : rawdata){
             line = lines.split(",");
             if (line[0].toLowerCase().equals(id.toLowerCase())){
+                toreturn = line;
                 break;
             }
         }
-        return line;
+        return toreturn;
+    }
+    
+    public String displayOneSaleById(String id){
+        String[] onesale = readSalesbyid(id);
+        if (Objects.isNull(onesale)){
+            System.out.println("No sale record is found with that ID!");
+            return null;
+        }
+        else{
+            String itemname = fetchItemNameFromId(onesale[1]);
+            return String.format("SalesID: %s, Item Name: %s, Quantity: %s, Sale Date: %s", onesale[0], itemname, onesale[2], onesale[3]);
+        }
     }
     
     //Method to read all sales entry
@@ -91,7 +108,7 @@ public class SalesEntry implements IDGenerator{
     }
     
     //Private method to get the item name
-    private String fetchItemNameFromId(String id){
+    protected String fetchItemNameFromId(String id){
         List<String> rawdata = new ArrayList<>();
         String itemname = null;
         //read the item file
